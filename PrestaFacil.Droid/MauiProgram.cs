ffd7bfb3be198a75581bf53fbcbd491ff79dd5dd
Edwin Marte Torres <kdgpro15@gmail.com>
2024-06-14 11:06:06 -0400

    feature: Initial State and ignoring .idea folder on Git

---
+++ PrestaFacil.Droid/MauiProgram.cs
@@ -0,0 +1,14 @@
+﻿namespace PrestaFacil.Droid;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+
+        builder
+            .UseSharedMauiApp();
+
+        return builder.Build();
+    }
+}
---
+++ PrestaFacil/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PrestaFacil;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
